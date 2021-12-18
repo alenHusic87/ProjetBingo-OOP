2dd69c32e7785cfa1285195c355f14be07ded045
alenHusic87 <92122155+alenHusic87@users.noreply.github.com>
2021-12-18 16:03:28 -0500

    Dev-AlenHusic-Bingo-OOP

---
+++ ProjetBingo-main/ProjetJeuPOO/Controller.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using ProjetJeuPOO.Bingo;
+using ProjetJeuPOO.SimiliBlackJack;
+using ProjetJeuPOO.SimiliPendu;
+
+namespace ProjetJeuPOO
+{
+    class Controller
+    {
+        static void Main(string[] args)
+        {
+            Boulier a = new Boulier();
+        // BlackJackController gameBlackJack = new BlackJackController();
+        }
+    }
+}
